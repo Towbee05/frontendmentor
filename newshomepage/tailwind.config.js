ac647ea41b65e3ad858a5bbe50c6733cfbe3465f
Towbee05 <olatiseoluwatobiloba@gmail.com>
2024-07-30 10:52:09 +0100

    news homepage

---
+++ newshomepage/tailwind.config.js
@@ -0,0 +1,37 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./**/*.{html,js}"
+  ],
+  theme: {
+    extend: {
+      screens:{
+        "desktop" : {
+          "min" :"700px"
+        }
+      },
+
+      colors: {
+        "custom-soft-orange" : "#e9ab53",
+        "custom-soft-red" : "#f15e50",
+        "custom-off-white" : "#fffdfa",
+        "custom-greyish-blue" : "#c5c6ce",
+        "custom-dark-greyish-blue" : "#5d5f79",
+        "custom-very-dark-blue" : "#00001a",
+      },
+
+      fontFamily: {
+        "custom-inter" : '"Inter", sans-serif'
+      },
+
+      fontSize: {
+        "15" : ["15px", "26px"]
+      },
+      gridTemplateColumns:{
+        "grid-custom" : "repeat(auto-fit)"
+      }
+    },
+  },
+  plugins: [],
+}
+
