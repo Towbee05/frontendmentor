42fe73df813b03271b0214337befa9fb1d6ee1c8
Towbee05 <olatiseoluwatobiloba@gmail.com>
2024-09-21 01:31:58 +0100

    initial commit => bookmark

---
+++ bookmark/tailwind.config.js
@@ -0,0 +1,41 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    './index.html'
+  ],
+  theme: {
+    extend: {
+      fontFamily: {
+        "rubiks" : '"Rubik", sans-serif'
+      },
+      colors: {
+        'custom-soft-blue' : 'hsl(231, 69%, 60%)',
+        'custom-soft-red' : 'hsl(0, 94%, 66%)',
+        'custom-greyish-blue' : 'hsl(229, 8%, 60%)',
+        'custom-dark-blue' : 'hsl(229, 31%, 21%)'
+      },
+      fontSize: {
+        "12px" : ['12px', '40px'],
+        '14px' : ['14px', '28px'],
+        '15px' : ['15px', '25px'],
+        '15px2.0' : ['15px', '32px'],
+        '15px2.0' : ['15px', '30px'],
+        '24px' : ['24px', '52px'],
+        '24px2.0' : ['24px', '28px'],
+        '30px' : ['30px', '40px'],
+        '32px' : ['32px', '52px'],
+        '48px' : ['48px', '52px']
+      },
+      padding: {
+        '10px' : '10px'
+      },
+      screens: {
+        laptop: {
+          min : '900px'
+        }
+      }
+    },
+  },
+  plugins: [],
+}
+
