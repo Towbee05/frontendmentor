aac312a5e99ab4750c96f11cecfa9ad3c12632e5
Towbee05 <olatiseoluwatobiloba@gmail.com>
2024-10-02 00:55:08 +0100

    finished up settings functionalities

--- pomodoro/scripts/app.js
+++ pomodoro/scripts/app.js
@@ -5,10 +5,14 @@ const settingContainer = getSingleElement('.settings-container');
 const openSettings = getSingleElement('#open-settings');
 const closeSettings = getSingleElement('#close-settings');
 const submitBtn = getSingleElement('.submit-btn');
-const inputContainer = getMultipleElements('.input-container');
+const input = getMultipleElements('input[type="number"]');
 const arrowUpBtns = getMultipleElements('.increase-btn');
 const arrowDownBtns = getMultipleElements('.decrease-btn');
 const typeBtn = getMultipleElements('.type-btn');
+const fonts = getMultipleElements('.fonts');
+const fontsParagraph = getMultipleElements('.fonts-paragraph');
+const colors = getMultipleElements('.color');
+const colorsInner = getMultipleElements('.colors-inner');
 
 // ! Activate the current type picked by the user 
 typeBtn.forEach((btn) => {
@@ -23,6 +27,14 @@ openSettings.addEventListener('click', () => removeClass(settingContainer, 'hidd
 closeSettings.addEventListener('click', () => addClass(settingContainer, 'hidden')); 
 
 // ! Increase and decrease number based on arrow click
+input.forEach((tag) => {
+    tag.addEventListener('keyup', () => {
+        if (Number(tag.value) < 0) {
+            tag.value = Math.abs(Number(tag.value))
+        }
+    });
+})
+
 arrowUpBtns.forEach((btn) => {
     btn.addEventListener('click', (e) => {
         const inputTag = e.currentTarget.parentElement.previousElementSibling;
@@ -33,7 +45,25 @@ arrowUpBtns.forEach((btn) => {
 arrowDownBtns.forEach((btn) => {
     btn.addEventListener('click', (e) => {
         const inputTag = e.currentTarget.parentElement.previousElementSibling; 
-        if (inputTag.value < 1) inputTag.value = 0;
-        else inputTag.value = Number(inputTag.value) - 1;
+        inputTag.value = Number(inputTag.value) - 1;
+        if (inputTag.value < 1) inputTag.value = 1;
+    });
+});
+
+// ! Select font
+fonts.forEach((btn) => {
+    btn.addEventListener('click', () => {
+        fontsParagraph.forEach((para) => removeClass(para, 'selected-font'));
+        const paragraph = btn.querySelector('p');
+        addClass(paragraph, 'selected-font');    
+    }); 
+});
+
+// ! select color
+colors.forEach((btn) => {
+    btn.addEventListener('click', () => {
+        colorsInner.forEach((color) => removeClass(color, 'selected-color'));
+        const colorsSpan = btn.querySelector('.colors-inner');
+        addClass(colorsSpan, 'selected-color');
     });
 });
